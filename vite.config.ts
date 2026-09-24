8ff16bba6a659f77e7f54b93522fbfebcdbf6820
Marcos Emanuel <marcosemanoelnunessoares@gmail.com>
2026-09-24 19:39:01 -0300

    Atualização do SGB

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { readExecutionProfile } from "./scripts/execution-profile.mjs";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "29696181-91a9-4521-a60a-6d51950ff9d3";
 
 const { d1, r2 } = hostingConfig;
 
@@ -20,7 +20,7 @@ const localBindingConfig = {
     ? [
         {
           binding: d1,
-          database_name: "site-creator-d1",
+          database_name: "sgb-db",
           database_id: SITE_CREATOR_PLACEHOLDER_DATABASE_ID,
         },
       ]
